d879167df6b4a3f0b4144613395a1a2a69661ca3
Robin Khiv <robinkhiv@gmail.com>
2019-01-16 09:27:22 -0800

    iterating with for IN loop

--- object_drills.js
+++ object_drills.js
@@ -1,3 +1,4 @@
+//loaf
 const loaf={
     flour: 300,
     water: 210,
@@ -7,3 +8,21 @@ const loaf={
 }
 console.log(loaf.flour , loaf.water);
 console.log(loaf.hydration())
+
+//iterating over an objet prop
+const obj = {
+    foo: 'asdfd',
+    bar: 'bar',
+    fum: 'fum',
+    quux: 'quux',
+    spam: 'spam'
+}
+function loop (obj){
+    for (let key in obj){
+        console.log(`${key} = ${obj[key]}`)
+    } 
+}
+
+loop(obj);
+
+//
